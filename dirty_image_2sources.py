cf71d81e4d7f729a3d9f3d42a25c1bdfa5404bdc
vlad7235 <vlad.stolyarov@gmail.com>
2015-02-23 17:45:28 +0000

    Simple illustration of the dirty image construction is included - dirty_image_2sources.py

--- dirty_image_2sources.py
+++ dirty_image_2sources.py
@@ -11,54 +11,84 @@ from matplotlib import pylab
 import matplotlib.pyplot as pyplot
 from mpl_toolkits.mplot3d import Axes3D
 
-def aaf_ns(a, m, c):
-    """
-
-    """
-    r=numpy.hypot(*ucs(a))
-    return scipy.special.pro_ang1(m,m,c,r)
-
-
 
 if 1:
+    """
+    Read antenna positions from ASCII file (XYZ, meters)
+    """
     vlas=numpy.genfromtxt("/home/vlad/software/SKA/crocodile/test/VLA_A_hor_xyz_5ants.txt", delimiter=",")
     pyplot.scatter(vlas[:,0], vlas[:,1])
     pyplot.title('Antenna positions on the (X,Y) plane')
     pyplot.xlabel('X, meters')
     pyplot.ylabel('Y, meters')
+    pyplot.grid()
     pyplot.show()
 #    raw_input("press enter")
 
+    """
+    Generate UV plane coverage for a batch of the snapshots taken every 22.9 minutes (0.1 radians)
+    during 12 hours (Pi radians) at 90 deg declination (Pi/2), wavelength is 5 meters.
+    """
     uvstep = 0.1
     wl = 5.0
-    vobs=genuv(vlas, numpy.arange(0,numpy.pi,uvstep) ,  numpy.pi/2.0)
+    dec = numpy.pi/2.0
+    obsTime = numpy.pi
+    vobs=genuv(vlas, numpy.arange(0,obsTime,uvstep) ,  dec)
     pyplot.cla()
     pyplot.scatter(vobs[:,0]/wl, vobs[:,1]/wl)
     pyplot.title('UV plane coverage for 12h observation, wavelength = 5m')
     pyplot.xlabel('U, number of wavelengths')
     pyplot.ylabel('V, number of wavelengths')
+    pyplot.grid()
     pyplot.show()
 #    raw_input("press enter")
 
+    """
+    Generate visibilities from two point sources of unit intensity, located
+    at (l1,m1) = (0.01 rad, 0.01 rad) and (l2,m2) = (-0.01 rad, -0.01 rad)
+    w.r.t. the phase center
+
+    """
     yy=genvis(vobs/wl, 0.01, 0.01)
     yy=yy + genvis(vobs/wl, -0.01, -0.01)
 
-# Fill the conjugated part V(-u,-v) = V*(u,v)
+
+    """
+    Fill the Hermitian conjugated part of the UV plane, V(-u,-v) = V*(u,v)
+    """
     vobs_tmp = numpy.copy(vobs)
     vobs_tmp[:,0] *= -1.0
     vobs_tmp[:,1] *= -1.0
     vobs_new = numpy.concatenate((vobs, vobs_tmp))
+    yy_tmp = numpy.conjugate(yy)
+    yy_new = numpy.concatenate((yy, yy_tmp))
+
     pyplot.cla()
     pyplot.scatter(vobs_new[:,0]/wl, vobs_new[:,1]/wl)
     pyplot.title('UV plane coverage for 12h observation with Hermitian part')
     pyplot.xlabel('U, number of wavelengths')
     pyplot.ylabel('V, number of wavelengths')
+    pyplot.grid()
     pyplot.show()
 #    raw_input("press enter")
 
-    yy_tmp = numpy.conjugate(yy)
-    yy_new = numpy.concatenate((yy, yy_tmp))
 
+    
+    pyplot.cla()
+    pyplot.scatter((0.01, -0.01), (0.01, -0.01))
+    pyplot.title('A model of two point sources with unit intensity')
+    pyplot.xlabel('l, radians')
+    pyplot.ylabel('m, radians')
+    pyplot.grid()
+    pyplot.show()
+
+
+    """
+    Transfer visibilities to the square matrix nmat x nmat using
+    re-gridding routine (no visibility stacking in UV cells, correct for the 
+    sparse UV plane coverage)
+
+    """
     nmat = 512
     mat_a = numpy.zeros((nmat,nmat),'D')
     maxvobs = max(vobs_new[:,0:1])[0] + 1
@@ -72,12 +102,14 @@ if 1:
     pyplot.show()
 #    raw_input("press enter")
 
-
 #    pyplot.cla()
 #    pyplot.imshow(numpy.abs(mat_a))
 #    pyplot.show()
 #    raw_input("press enter")
 
+    """
+    Apply FFTSHIFT to shift the phase center to the origin
+    """
     mat_b = numpy.fft.ifftshift(mat_a)
     pyplot.cla()
     pyplot.contour(numpy.abs(mat_b))
@@ -88,18 +120,28 @@ if 1:
     pyplot.show()
 #    raw_input("press enter")
 
+
+    """
+    Make inverse Fourier-transform to get the dirty image and apply FFTSHIFT again
+    to return the phase center to the middle of the matrix
+    (should be done by checkboarding of the Fourier image - ToDo)
+    """
     c = numpy.fft.ifft2(mat_b)
     c1 = numpy.fft.fftshift(c)
     pyplot.cla()
-    pyplot.contour(numpy.abs(c1))
+    pyplot.contourf(numpy.abs(c1))
     pyplot.title('Dirty image of the two point sources recovered')
     pyplot.xlabel('l, number of pixel')
     pyplot.ylabel('m, number of pixel')
+    pyplot.colorbar(label='I(l,m)')
     pyplot.show()
 #    raw_input("press enter")
     
 
 
+    """
+    Smooth and decimate the dirty image for 3D surface plot	
+    """
     ncomp = 4
     c2 = scipy.ndimage.gaussian_filter(abs(c1), ncomp)
     c3 = c2[::ncomp,::ncomp]
@@ -116,21 +158,5 @@ if 1:
     pyplot.title('3D surface of the dirty image recovered (smoothed)')
     pyplot.xlabel('l, number of pixel')
     pyplot.ylabel('m, number of pixel')
-   # pyplot.zlabel('I(l,m), amplitude')
- 
     pyplot.show()
 
-if 0:
-    majorcycle(0.025, 15000, vobs/5 , yy, 0.1, 5, 100, 250000)
-    
-
-if 0: # some other testing code bits
-    mg=exmid(numpy.fft.fftshift(numpy.fft.fft2(aaf(a, 0, 3))),5)
-    ws=numpy.arange( p[:,2].min(), p[:,2].max(), wstep)
-    wr=zip(ws[:-1], ws[1:]) + [ (ws[-1], p[:,2].max() ) ]
-    yy=genvis(vobs/5, 0.001, 0.001)
-    d,p=doimg(0.025, 15000, vobs/5, yy, lambda *x: wslicimg(*x, wstep=250))
-    pylab.matshow(p[740:850,740:850]); pylab.colorbar(); pylab.show()
-    x=numpy.zeros_like(d)
-    x[1050,1050]=1
-    xuv=numpy.fft.fftshift(numpy.fft.fft2(numpy.fft.ifftshift(x)))
