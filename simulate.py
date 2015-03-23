746d114c5d1b4a568a676cfb3423beb9939fc257
Anna Scaife <a.scaife@soton.ac.uk>
2015-03-23 16:08:05 +0200

    parameters renamed, comments added

--- simulate.py
+++ simulate.py
@@ -1,43 +1,125 @@
 # Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
 # 
 # Simulate data observed by an interferometer
+#
+# ---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
+#
+# USEFUL NOTES
+#
+# From http://casa.nrao.edu/Memos/CoordConvention.pdf :
+#
+# UVW is a right-handed coordinate system, with W pointing towards the source, 
+# and a baseline convention of ant2 - ant1 where index(ant1) < index(ant2).
+# Consider an XYZ Celestial coordinate system centered at the location of the
+# interferometer, with X towards the East, Z towards the NCP and Y to complete a 
+# right-handed system. The UVW coordinate system is then defined by the hour-angle
+# and declination of the phase-reference direction such that (a) when the 
+# direction of observation is the NCP (ha=0,dec=90), the UVW coordinates are aligned
+# with XYZ, (b) V, W and the NCP are always on a Great circle, (c) when W is
+# on the local meridian, U points East (d) when the direction of observation is at 
+# zero declination, an hour-angle of -6 hours makes W point due East. The (l,m,n)
+# coordinates are parallel to (U,V,W) such that l increases with Right-Ascension 
+# (or increasing longitude coordinate), m increases with Declination, and n is towards
+# the source. With this convention, images will have Right Ascension increasing 
+# from Right to Left, and Declination increasing from Bottom to Top.
+#
+# ---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
+# 
+# [150323 - AMS]    Correction to rot(), introduced -1 factor in v calculation.
+#                   Change to bsl(), to agree with casa convention.
+#
+# ---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
+
 
 import numpy
 
-def rot(p, ha, dec):
-    """
-    Rotate x,y,z in earth coordinates to u,v,w relative to direction
-    defined by ha and dec
+def rot(ants_xyz, ha, dec):
 
-    p: array of antenna positions
     """
-    x,y,z=numpy.hsplit(p,3)
+    Rotate (x,y,z) antenna positions in earth coordinates 
+    to (u,v,w) coordinates relative to 
+    astronomical source position (ra, dec).
+
+    (see note on co-ordinate systems in header)
+    """
+
+    # ants_xyz : (x,y,z) co-ordinates of antennas in array
+    # ha : hour angle of source (ha = ra - lst)
+    # dec : declination of source
+
+    x,y,z=numpy.hsplit(ants_xyz,3)
+
     t=x*numpy.cos(ha) - y*numpy.sin(ha)
     u=x*numpy.sin(ha) + y*numpy.cos(ha)
-    v=t*numpy.sin(dec)+ z*numpy.cos(dec)
+    v=-1.*t*numpy.sin(dec)+ z*numpy.cos(dec)
     w=t*numpy.cos(dec)+ z*numpy.sin(dec)
-    return numpy.hstack([u,v,w])
 
-def bls(p):
+    ants_uvw = numpy.hstack([u,v,w])
+
+    return ants_uvw
+
+
+def bls(ants_uvw):
+
     """
-    Compute baseline uvws from station uvw
+    Compute baselines in uvw co-ordinate system from 
+    uvw co-ordinate system station positions
     """
+
+    # ants_uvw : (u,v,w) co-ordinates of antennas in array
+    # basel_uvw : delta-(u,v,w) values for baseline
+    
     res=[]
-    for i in range(p.shape[0]):
-        for j in range(i+1, p.shape[0]):
-            res.append(p[i]-p[j])
-    return numpy.array(res)
+    for i in range(ants_uvw.shape[0]):
+        for j in range(i+1, ants_uvw.shape[0]):
+            res.append(ants_uvw[j]-ants_uvw[i])
 
-def genuv(p, ha, dec):
-    """Generate UVW coordianates given a sequence of antenna uvw positions
-    (p), a sequence of hour angles (ha) and a decliation (dec)
+    basel_uvw = numpy.array(res)
+
+    return basel_uvw
+
+
+def genuv(ants_xyz, ha_range, dec):
 
     """
-    return(numpy.concatenate([bls(rot(p,hax,dec)) for hax in ha]))
+    Calculate baselines in (u,v,w) co-ordinate system 
+    for a range of hour angles (i.e. non-snapshot observation)
+    to create a uvw sampling distribution
+    """
 
-def genvis(p, l, m):
-    """Simulate visibilities for point source at (l,m) at uvw coordinates in
-    (p)"""
+    # ants_xyz : (x,y,z) co-ordinates of antennas in array
+    # ha_range : list of hour angle values for astronomical source as function of time
+    # dec : declination of astronomical source [constant, not f(t)]
+    # dist_uvw : (u,v,w) distribution of projected baselines
+
+    dist_uvw = numpy.concatenate([bls(rot(ants_xyz,hax,dec)) for hax in ha_range])
+
+    return dist_uvw
+
+
+def genvis(dist_uvw, l, m):
+
+    """
+    Simulate visibilities for unit amplitude point source at 
+    angular position (l,m) relative to observed phase centre 
+    defined at (ra,dec).
+
+    Note that point source is delta function, therefore
+    FT relation ship becomes an exponential, evaluated at 
+    (uvw.lmn)
+    """
+
+    # dist_uvw : (u,v,w) distribution of projected baselines
+    # l : direction cosine relative to phase centre defined at (ra,dec)
+    # m : orthogonal directon cosine relative to phase centre defined at (ra,dec)
+    # s : (l,m,n) vector direction to source 
+    # vis : complex valued visibility data 
+
     s=numpy.array([l, m , numpy.sqrt(1 - l**2 - m**2)])
-    return numpy.exp(-2j*numpy.pi* numpy.dot(p, s))
+    vis = numpy.exp(-2j*numpy.pi* numpy.dot(dist_uvw, s))
+
+    return vis
 
