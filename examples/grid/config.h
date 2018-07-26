e7c0edc4a48d9732f5ce54875c126055e30c1c18
Peter Wortmann <pw410@cam.ac.uk>
2018-07-26 21:15:33 +0100

    Move configuration routines into own file
    Working towards merging this with the distributed program

--- examples/grid/config.h
+++ examples/grid/config.h
@@ -34,7 +34,7 @@ struct subgrid_work
 {
     int iu, iv;
     double d_u, d_v;
-	int subgrid_off0, subgrid_off1;
+	int subgrid_off_u, subgrid_off_v;
 	int start_bl;
     int nbl;
 	struct subgrid_work_bl *bls;
@@ -63,5 +63,17 @@ void bl_bounding_box(struct vis_spec *spec, int a1, int a2,
                      double *uvw_l_min, double *uvw_l_max);
 void bl_bounding_subgrids(struct vis_spec *spec, double lam, double xA, int a1, int a2,
                           int *sg_min, int *sg_max);
+
+bool work_config_set(struct work_config *cfg,
+                     struct vis_spec *spec,
+                     int facet_workers, int subgrid_workers,
+                     double theta, int image_size, int subgrid_spacing,
+                     char *pswf_file,
+                     int yB_size, int yN_size, int yP_size,
+                     int xA_size, int xM_size, int xMxN_yP_size);
+
+void vis_spec_to_bl_data(struct bl_data *bl, struct vis_spec *spec,
+                         int a1, int a2);
+bool create_bl_groups(hid_t vis_group, struct work_config *work_cfg, int worker);
 
 #endif // CONFIG_H
