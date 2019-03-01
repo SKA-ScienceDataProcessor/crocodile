b59d21928fd92da31485419cfcbd6edf90bb0510
Peter Wortmann <pw410@cam.ac.uk>
2019-03-01 15:31:22 +0000

    Only write overlapping chunks in test_config
    This should make the I/O behaviour actually equivalent to the main benchmark.
    
    # Conflicts:
    #	examples/grid/test_config.c

--- src/test_config.c
+++ src/test_config.c
@@ -16,6 +16,7 @@ void simple_benchmark(const char *filename,
 
     struct subgrid_work *work = work_cfg->subgrid_work +
         worker*work_cfg->subgrid_max_work;
+    struct vis_spec *spec = &work_cfg->spec;
 
     // Some test data to write
     int j;
@@ -35,10 +36,21 @@ void simple_benchmark(const char *filename,
     fflush(stdout);
 
     // Run through all work we are supposed to do, writing data
-    uint64_t bytes = 0;
+    uint64_t bytes = 0, skipped_bytes = 0;
     int iwork;
+    int ntchunk = (spec->time_count + spec->time_chunk - 1) / spec->time_chunk;
+    int nfchunk = (spec->freq_count + spec->freq_chunk - 1) / spec->freq_chunk;
+    double lam = (double)work_cfg->recombine.image_size / work_cfg->theta;
+    double xA = (double)work_cfg->recombine.xA_size / work_cfg->recombine.image_size;
+    printf("lam = %g, xA = %g\n");
     for (iwork = 0; iwork < work_cfg->subgrid_max_work; iwork++) {
-        if (!work[iwork].nbl) continue; 
+        if (!work[iwork].nbl) continue;
+
+        double sg_min_u = lam * (xA*work[iwork].iu - xA/2);
+        double sg_min_v = lam * (xA*work[iwork].iv - xA/2);
+        double sg_max_u = lam * (xA*work[iwork].iu + xA/2);
+        double sg_max_v = lam * (xA*work[iwork].iv + xA/2);
+
         struct subgrid_work_bl *wbl;
         printf("%d ", iwork); fflush(stdout);
         // Loop through baselines
@@ -51,13 +63,30 @@ void simple_benchmark(const char *filename,
             // written multiple times. This will also happen in
             // reality, just not as often.
             int itime, ifreq;
-            for (itime = 0; itime < work_cfg->spec.time_count / time_chunk; itime++)
-                for (ifreq = 0; ifreq < work_cfg->spec.freq_count / freq_chunk; ifreq++) {
-                    if (write)
-                        write_vis_chunk(vis_g, &bl, time_chunk, freq_chunk, itime, ifreq, data);
-                    else
-                        read_vis_chunk(vis_g, &bl, time_chunk, freq_chunk, itime, ifreq, data);
-                    bytes += sizeof(double complex) * time_chunk * freq_chunk;
+            for (itime = 0; itime < ntchunk; itime++)
+                for (ifreq = 0; ifreq < nfchunk; ifreq++) {
+
+                    double uvw_l_min[3], uvw_l_max[3];
+                    bl_bounding_box(spec, wbl->a1, wbl->a2,
+                                    itime * spec->time_chunk,
+                                    fmax(spec->time_count, (itime+1) * spec->time_chunk) - 1,
+                                    ifreq * spec->freq_chunk,
+                                    fmin(spec->freq_count, (ifreq+1) * spec->freq_chunk) - 1,
+                                    uvw_l_min, uvw_l_max);
+
+                    if ((uvw_l_min[0] < sg_max_u && uvw_l_max[0] > sg_min_u &&
+                         uvw_l_min[1] < sg_max_v && uvw_l_max[1] > sg_min_v) ||
+                        (-uvw_l_max[0] < sg_max_u && -uvw_l_min[0] > sg_min_u &&
+                         -uvw_l_max[1] < sg_max_v && -uvw_l_min[1] > sg_min_v)) {
+
+                        if (write)
+                            write_vis_chunk(vis_g, &bl, time_chunk, freq_chunk, itime, ifreq, data);
+                        else
+                            read_vis_chunk(vis_g, &bl, time_chunk, freq_chunk, itime, ifreq, data);
+                        bytes += sizeof(double complex) * time_chunk * freq_chunk;
+                    } else {
+                        skipped_bytes += sizeof(double complex) * time_chunk * freq_chunk;
+                    }
                 }
             free(bl.time); free(bl.freq); free(bl.uvw_m);
         }
@@ -103,19 +132,6 @@ int main(int argc, char *argv[])
     load_ant_config("../data/grid/LOWBD2_north_cfg.h5", &cfg);
     //load_ant_config("../data/grid/VLAA_north_cfg.h5", &cfg);
 
-    struct vis_spec spec;
-    spec.cfg = &cfg;
-    spec.fov = 0.1;
-    spec.dec = 90 * atan(1) * 4 / 180;
-    spec.time_start = 10 * -45 / 3600; // h
-    spec.time_count = 512;
-    spec.time_chunk = 64;
-    spec.time_step = 0.9 / 3600; // h
-    spec.freq_start = 225e6; // Hz
-    spec.freq_count = 4096;
-    spec.freq_chunk = 64;
-    spec.freq_step = 75.e6 / spec.freq_count; // Hz
-
     struct work_config work_cfg;
     config_init(&work_cfg);
     if (!config_set(&work_cfg,
@@ -125,6 +141,20 @@ int main(int argc, char *argv[])
                     704, 1024, 146)) {
         return 1;
     }
+
+    struct vis_spec spec;
+    spec.cfg = &cfg;
+    spec.fov = (double)work_cfg.recombine.image_size / 210000;
+    spec.dec = 90 * atan(1) * 4 / 180;
+    spec.time_start = -230 / 3600; // h
+    spec.time_count = 256;
+    spec.time_chunk = 64;
+    spec.time_step = -460 / 3600 / spec.time_count; // h
+    spec.freq_start = 225e6; // Hz
+    spec.freq_count = 4096;
+    spec.freq_chunk = 64;
+    spec.freq_step = 75.e6 / spec.freq_count; // Hz
+
     config_set_visibilities(&work_cfg, &spec, spec.fov / 2 / 0.4, NULL);
     config_assign_work(&work_cfg, 9, subgrid_workers);
 
@@ -150,7 +180,7 @@ int main(int argc, char *argv[])
         }
 
         // Create all baseline groups
-        create_bl_groups(vis_g, &work_cfg, world_size > 1 ? -1 : i);
+        create_bl_groups(vis_g, &work_cfg, world_size > 1 ? i : -1);
         H5Gclose(vis_g); H5Fclose(vis_f);
 
         // Run simple write+read benchmark
@@ -158,5 +188,10 @@ int main(int argc, char *argv[])
         simple_benchmark(filename, &work_cfg, false, i);
 
     }
+
+#ifndef NO_MPI
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+#endif
     return 0;
 }
